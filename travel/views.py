573d320d710380be2530a2f661dc7333303c3cfa
Peter McCurdy <petermccurdy@alumni.uwaterloo.ca>
2008-12-25 12:16:31 -0330

    Make the site actually work with Django.
    Since we don't really get any command-line parameters any more, put the
    Google Maps key / key file info in settings.py.

--- travel/views.py
+++ travel/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.conf import settings
 
 import datetime
+import simplejson
 import time
 
-from routez.travel.models import Map
+from routez.travel.models import Route, Stop, Map
 
 # Returns the hours and minutes of the given Unix timestamp, formatted
 # nicely.  If the timestamp is not given, defaults to the current time.
@@ -29,30 +31,34 @@ def main_page(request):
     return render_to_response('index.html', 
         {'min_lat': m.min_lat, 'min_lon': m.min_lng, 
             'max_lat': m.max_lat, 'max_lon': m.max_lng, 
-            'key': "", 'now': " " + now_str})
+            'key': settings.GMAPS_API_KEY, 'now': " " + now_str})
 
 def stoplist(request):
     matches = []
     for s in Stop.objects.all():
-        matches.append({ 'id':"gtfs"+s.stop_id, 'name':s.name, 
-                        'lat':s.lat, 'lng':s.lng })
-    return request.respond(asJSON(matches))
+        matches.append({'id': "gtfs" + s.stop_id, 
+                        'name': s.name, 
+                        'lat': s.lat, 
+                        'lng': s.lng })
+    return HttpResponse(simplejson.dumps(matches), mimetype="application/json")
 
 def routelist(request):
     matches = []
     for r in Route.objects.all():
         matches.append({ 'id':r.route_id, 'shortname':r.short_name, 
                        'longname':r.long_name } )
-    return request.respond(asJSON(matches))
+    return HttpResponse(simplejson.dumps(matches), mimetype="application/json")
 
 def routeplan(request):
-    start_lat = float(request.get('startlat'))
-    start_lng = float(request.get('startlng'))
-    end_lat = float(request.get('endlat'))
-    end_lng = float(request.get('endlng'))
-    time_str = request.get('time') or ""
+    start_lat = float(request.GET['startlat'])
+    start_lng = float(request.GET['startlng'])
+    end_lat = float(request.GET['endlat'])
+    end_lng = float(request.GET['endlng'])
+    time_str = request.GET.get('time', "")
 
-    start_time = self.calendar.parse(time_str)[0]
+    import parsedatetime.parsedatetime as pdt
+    calendar = pdt.Calendar()
+    start_time = calendar.parse(time_str)[0]
     daysecs = time.mktime((start_time[0], start_time[1], start_time[2],
                 0, 0, 0, 0, 0, 0))
     now = datetime.datetime.fromtimestamp(time.mktime(start_time))
@@ -64,7 +70,12 @@ def routeplan(request):
     elif now.weekday() == 6:
         service_period = 'sunday'
 
-    trippath = self.graph.find_path(today_secs, service_period, 
+    # FIXME: We should find a way to load this only once, instead of for each 
+    # request.
+    from tripgraph import TripGraph
+    graph = TripGraph()
+    graph.load(settings.GRAPH_FILE)
+    trippath = graph.find_path(today_secs, service_period, 
                     start_lat, start_lng, end_lat, end_lng, None)
 
     actions_desc = []
@@ -74,9 +85,8 @@ def routeplan(request):
         # then move
         if last_action and last_action.route_id != action.route_id:
             if last_action.route_id != -1:
-                ts = self.graph.get_tripstop(last_action.dest_id)
-                action_time = Main.human_time(
-                    daysecs + last_action.start_time)
+                ts = graph.get_tripstop(last_action.dest_id)
+                action_time = human_time(daysecs + last_action.start_time)
                 actions_desc.append({ 'type':'alight', 
                                     'lat': ts.lat, 
                                     'lng': ts.lng, 
@@ -84,12 +94,12 @@ def routeplan(request):
 
         if not last_action or last_action.route_id != action.route_id:
             if action.route_id >= 0:
-                action_time = Main.human_time(daysecs + action.start_time)
+                action_time = human_time(daysecs + action.start_time)
                 actions_desc.append({ 'type':'board', 'id':action.src_id, 
                                     'time': action_time,
                                     'route_id':action.route_id })
 
-        ts = self.graph.get_tripstop(action.src_id)
+        ts = graph.get_tripstop(action.src_id)
         actions_desc.append({ 'type':'pass', 'id':action.src_id, 
                             'lat': ts.lat, 
                             'lng': ts.lng,
@@ -98,8 +108,9 @@ def routeplan(request):
 
     # if we had a path at all, append the last getting off action here
     if last_action:
-        action_time = Main.human_time(daysecs + last_action.end_time)
+        action_time = human_time(daysecs + last_action.end_time)
         actions_desc.append({ 'type': 'arrive', 
                               'time': action_time })
 
-    return request.respond(asJSON(actions_desc))
+    return HttpResponse(simplejson.dumps(actions_desc), 
+        mimetype="application/json")
