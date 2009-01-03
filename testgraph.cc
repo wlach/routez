29c72f134826b7efe1a252cb0b2ff864c8571ded
Peter McCurdy <petermccurdy@alumni.uwaterloo.ca>
2009-01-03 04:39:58 -0330

    Dump the returned path info (in reverse) in testgraph.cc.

--- testgraph.cc
+++ testgraph.cc
@@ -5,6 +5,14 @@ int main(int argc, char *argv[])
     TripGraph g;
     g.load("mygraph.routez");
 
-    g.find_path(46167, "weekday", false, 44.657304, -63.591096, 
+    TripPath p = g.find_path(46167, "weekday", false, 44.657304, -63.591096, 
                 44.7321379, -63.657304, NULL);
+    boost::shared_ptr<TripAction> a(p.last_action);
+    while (a)
+    {
+        printf("%s->%s; route: %d; start time: %.2f; end time: %.2f\n", 
+            a->src_id.c_str(), a->dest_id.c_str(), a->route_id, 
+            a->start_time, a->end_time);
+        a = a->parent;
+    }
 }
