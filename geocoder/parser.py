6b326bfdfc17d0f494bd7f273dfa45a4c6aff152
William Lachance <wrlach@gmail.com>
2009-03-16 14:41:38 -0300

    Better intersection parsing, taking region into account

--- geocoder/parser.py
+++ geocoder/parser.py
@@ -34,11 +34,14 @@ type_ = Combine( oneOf("Street St Boulevard Blvd Lane Ln Road Rd Avenue Ave "
 # region
 region_ = oneOf("Dartmouth Halifax", caseless=True)
 
+# join string
+and_ = oneOf("and &", caseless=True)
+
 # street name 
 streetName = (Combine( Optional(oneOf("N S E W")) + number + 
                         Optional("1/2") + 
                         Optional(numberSuffix), joinString=" ", adjacent=False ) 
-              | Combine(OneOrMore(~type_ + ~region_ + name), joinString=" ",adjacent=False) )
+              | Combine(OneOrMore(~type_ + ~region_ + ~and_ + name), joinString=" ",adjacent=False) )
 
 # basic street address
 streetReference = streetName.setResultsName("name") + Optional(type_.setResultsName("type"))
@@ -46,26 +49,37 @@ streetReferenceWithRegion = streetReference + Optional(region_.setResultsName("r
 direct = number.setResultsName("number") + streetReference
 directWithRegion = number.setResultsName("number") + streetReferenceWithRegion
 streetAddress = ( directWithRegion.setResultsName("street") | 
-                  direct.setResultsName("street") |
-                  streetReferenceWithRegion.setResultsName("street") |
-                  streetReference.setResultsName("street") )
+                  streetReferenceWithRegion.setResultsName("street"))
+
+# intersection
+intersection = (streetReferenceWithRegion.setResultsName("street1") + \
+                and_ + \
+                (streetReferenceWithRegion.setResultsName("street2")))
 
 def __normalize_suffix(addr):
     tmpsuffix = string.lower(addr.suffix)
-
+    
     if __suffix_mapping.get(tmpsuffix):
         addr.suffix = __suffix_mapping[tmpsuffix]
 
 
+class Address:
+    pass
+
 def parse_address(location_str):
+    # strip out and commas from the string, to make it easier
+    # to parse out the region (FIXME: better to do this in the parser?)
+    location_str = location_str.replace(",", "")
+
     location_strs = __splitre.split(location_str)
 
     if len(location_strs) == 1:
         addr = streetAddress.parseString(location_str)
         __normalize_suffix(addr)
-        return [ addr ]
+        
+        return [addr]
+
     elif len(location_strs) == 2:
-        addr1 = streetAddress.parseString(location_strs[0])
-        addr2 = streetAddress.parseString(location_strs[1])
-        return [ addr1, addr2]
+        inters = intersection.parseString(location_str)
+        return [inters.street1, inters.street2]
     
--- geocoder/tests.py
+++ geocoder/tests.py
@@ -16,15 +16,18 @@ class GeoParserTestCase(unittest.TestCase):
 
     def check_intersection(self, str, 
                            expected_name1, expected_number1, expected_suffix1, 
-                           expected_name2, expected_number2, expected_suffix2):
+                           expected_name2, expected_number2, expected_suffix2,
+                           expected_region):
         addr = geoparser.parse_address(str)
         self.assertEquals(len(addr), 2)
         self.assertEquals(addr[0].name, expected_name1)
         self.assertEquals(addr[0].number, expected_number1)
         self.assertEquals(addr[0].type, expected_suffix1)
+        self.assertEquals(addr[0].region, "")
         self.assertEquals(addr[1].name, expected_name2)
         self.assertEquals(addr[1].number, expected_number2)
         self.assertEquals(addr[1].type, expected_suffix2)
+        self.assertEquals(addr[1].region, expected_region)
 
     def testSingle(self):
         self.check_single("114 Brentwood", "Brentwood", "114", "", "")
@@ -33,7 +36,20 @@ class GeoParserTestCase(unittest.TestCase):
                           "Halifax")
         self.check_single("114 Brentwood Ave Halifax", "Brentwood", "114", 
                           "Ave", "Halifax")
+        self.check_single("114 Brentwood, Halifax", "Brentwood", "114", "", 
+                          "Halifax")
+        self.check_single("114 Brentwood Ave, Halifax", "Brentwood", "114", 
+                          "Ave", "Halifax")
         
     def testIntersection(self):
         self.check_intersection("North & Agricola", "North", "", "", 
-                                "Agricola", "", "")
+                                "Agricola", "", "", "")
+        self.check_intersection("North and Agricola", "North", "", "", 
+                                "Agricola", "", "", "")
+        self.check_intersection("North Street and Agricola", 
+                                "North", "", "Street", 
+                                "Agricola", "", "", "")
+        self.check_intersection("North Street and Agricola Halifax", 
+                                "North", "", "Street",
+                                "Agricola", "", "", 
+                                "Halifax")
