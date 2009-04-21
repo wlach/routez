763f62d747cc71aaabce3a4e4cef7bfd4598efb8
William Lachance <wrlach@gmail.com>
2009-04-21 16:37:19 -0300

    Fix stops in range to actually return a limited set of results

--- stop/views.py
+++ stop/views.py
@@ -70,26 +70,26 @@ def stoptimes_in_range(request, location, secs):
         distance_to_stop = (latlng[0] - ts.lat)**2 + (latlng[1] - ts.lng)**2
         for route_id in ts.get_routes(service_period):
             if not routehash.get(route_id) or \
-                    routehash[route_id][0] >= distance_to_stop:
+                    routehash[route_id][0] > distance_to_stop:
                 routehash[route_id] = (distance_to_stop, ts)
-
+                print "%s -> %s" % (route_id, distance_to_stop)
         
-        for route_id in routehash:
-            ts = routehash[route_id][1]
-            thops = ts.find_triphops(int(secs), int(route_id), service_period, 
+    for route_id in routehash:
+        ts = routehash[route_id][1]
+        thops = ts.find_triphops(int(secs), int(route_id), service_period, 
                                      3)              
-            times = []
-            if len(thops):
-                for thop in thops:
-                    times.append(thop.start_time)
-                stop = Stop.objects.filter(stop_id=ts.id)[0]
-                route = Route.objects.filter(route_id=route_id)[0]
-                routes.append({
-                        "stop_name": stop.name,
-                        "stop_code": stop.stop_code,
-                        "route_short_name": route.short_name,
-                        "route_long_name": route.long_name,
-                        "times": times })
+        times = []
+        if len(thops):
+            for thop in thops:
+                times.append(thop.start_time)
+            stop = Stop.objects.filter(stop_id=ts.id)[0]
+            route = Route.objects.filter(route_id=route_id)[0]
+            routes.append({
+                    "stop_name": stop.name,
+                    "stop_code": stop.stop_code,
+                    "route_short_name": route.short_name,
+                    "route_long_name": route.long_name,
+                    "times": times })
 
     return HttpResponse(simplejson.dumps({ 'routes': routes }), 
                         mimetype="application/json")
