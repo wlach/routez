55a585a9c8e613f6416e2df318ec79018c4059cc
William Lachance <wrlach@gmail.com>
2009-04-28 14:19:16 -0300

    Now stoptimes_in_range function, returning array of stops

--- stop/views.py
+++ stop/views.py
@@ -39,7 +39,10 @@ def stoptimes_for_stop(request, stop_code, secs):
             routes.append({ "short_name": route.short_name, "long_name": route.long_name,
                             "times": times })
 
-    return HttpResponse(simplejson.dumps([ {'name': stop.name, 'code': stop.stop_code, 'routes': routes } ]), 
+    return HttpResponse(simplejson.dumps({ "stops": [ {'name': stop.name, 
+                                                       'code': stop.stop_code,
+                                                       'routes': routes } ]
+                                           }),
                         mimetype="application/json")
 
 def stoptimes_in_range(request, location, secs):
@@ -63,33 +66,48 @@ def stoptimes_in_range(request, location, secs):
     # checking 500 meters. FIXME: make configurable
     tstops = graph.find_tripstops_in_range(latlng[0], latlng[1], TripStop.GTFS,
                                            500)
+
+    # filter twice, once to get all the routes we're interested in, then
+    # create a hash of stops containing only the routes that we care about
+    # FIXME: is this filtering better done on the client end of things? or 
+    # should it be an optional parameter?
     routehash = {}
-    route_id_thops = {}
-    routes = []
     for ts in tstops:
         distance_to_stop = (latlng[0] - ts.lat)**2 + (latlng[1] - ts.lng)**2
         for route_id in ts.get_routes(service_period):
             if not routehash.get(route_id) or \
                     routehash[route_id][0] > distance_to_stop:
                 routehash[route_id] = (distance_to_stop, ts)
-                print "%s -> %s" % (route_id, distance_to_stop)
-        
+
+    stophash = {}
     for route_id in routehash:
         ts = routehash[route_id][1]
-        thops = ts.find_triphops(int(secs), int(route_id), service_period, 
-                                     3)              
-        times = []
-        if len(thops):
-            for thop in thops:
-                times.append(thop.start_time)
-            stop = Stop.objects.filter(stop_id=ts.id)[0]
-            route = Route.objects.filter(route_id=route_id)[0]
-            routes.append({
-                    "stop_name": stop.name,
-                    "stop_code": stop.stop_code,
+        if not stophash.get(ts.id):
+            stophash[ts.id] = []
+        stophash[ts.id].append(route_id)
+
+    stopsjson = []
+    for id in stophash:
+        stop = Stop.objects.filter(stop_id=id)[0]
+        routesjson = []
+        for route_id in stophash[id]:
+            ts = graph.get_tripstop(id)
+            thops = ts.find_triphops(int(secs), int(route_id), service_period,
+                                     3)
+            if len(thops):
+                times = []
+                for thop in thops:
+                    times.append(thop.start_time)
+                route = Route.objects.filter(route_id=route_id)[0]
+                routejson = { 
                     "route_short_name": route.short_name,
                     "route_long_name": route.long_name,
-                    "times": times })
+                    "times": times }
+                routesjson.append(routejson)
+        stopsjson.append({ 
+                    "stop_name": stop.name,
+                    "stop_code": stop.stop_code,
+                    "routes": routesjson })
 
-    return HttpResponse(simplejson.dumps({ 'routes': routes }), 
+    return HttpResponse(simplejson.dumps({ 'stops': stopsjson }), 
                         mimetype="application/json")
