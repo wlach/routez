02c95daa9fb5703bbd0942fee2dcbdbebd296ff3
William Lachance <wrlach@gmail.com>
2009-08-12 19:35:20 -0400

    Adapt to service period changes in libroutez
    No new features in routez itself, though we get lots of bonus features
    (holiday support, software behaving sensibly after midnight) due to changes
    in libroutez.

--- stop/views.py
+++ stop/views.py
@@ -4,6 +4,7 @@ from django.conf import settings
 
 import datetime
 import simplejson
+import time
 
 from routez.travel.models import Route, Map, Shape
 from routez.stop.models import Stop
@@ -27,6 +28,41 @@ def latlng_dist(src_lat, src_lng, dest_lat, dest_lng):
     dist = math.degrees(math.acos(dist)) * (60.0 * 1.1515 * 1.609344 * 1000.0)
     return dist
 
+def get_route_ids_for_stop(graph, stop_id, secs):
+    ts = graph.get_tripstop(stop_id)
+
+    route_id_hash = {}
+
+    sptuples = graph.get_service_period_ids_for_time(int(secs))
+    for sptuple in sptuples:
+        route_ids = ts.get_routes(sptuple[0])
+        for route_id in route_ids:
+            route_id_hash[route_id] = 1
+
+    return route_id_hash.keys()
+
+def find_triphops_for_stop(graph, stop_id, route_id, secs, num):
+    t = time.localtime(float(secs))
+    elapsed_daysecs = t.tm_sec + (t.tm_min * 60) + (t.tm_hour * 60 * 60)
+
+    ts = graph.get_tripstop(stop_id)
+
+    all_triphops = []
+    sptuples = graph.get_service_period_ids_for_time(int(secs))
+    for sptuple in sptuples:
+        triphops = ts.find_triphops(elapsed_daysecs + sptuple[1], route_id,
+                                    sptuple[0], num)
+        for triphop in triphops:
+            triphop.start_time -= sptuple[1]
+            triphop.end_time -= sptuple[1]
+            all_triphops.append(triphop)
+
+    all_triphops.sort(lambda x, y: x.start_time-y.start_time)
+    if len(all_triphops) > num:
+        all_triphops = all_triphops[0:num]
+
+    return all_triphops
+
 def stoptimes_for_stop(request, stop_code, secs):
     stop = Stop.objects.filter(stop_code=stop_code)
     if not len(stop):
@@ -36,25 +72,20 @@ def stoptimes_for_stop(request, stop_code, secs):
     stop = stop[0]
     import routez
     graph = routez.travel.graph
-    today = datetime.date.today()
-    service_period = 'weekday'
-    if today.weekday() == 5:
-        service_period = 'saturday'
-    elif today.weekday() == 6:
-        service_period = 'sunday'
 
-    ts = graph.get_tripstop(int(stop.stop_id));
-    route_ids = ts.get_routes(service_period)
+    route_ids = get_route_ids_for_stop(graph, int(stop.stop_id), secs)
     routes = []
     for route_id in route_ids:
-        thops = ts.find_triphops(int(secs), int(route_id), service_period, 3)
+        thops = find_triphops_for_stop(graph, int(stop.stop_id), route_id,
+                                       secs, 3)
         times = []
         if len(thops):
             for thop in thops:
                 times.append(thop.start_time)
-            route = Route.objects.filter(route_id=route_id)[0]
-            routes.append({ "short_name": route.short_name, "long_name": route.long_name,
-                            "times": times })
+        route = Route.objects.filter(route_id=route_id)[0]
+        routes.append({ "short_name": route.short_name,
+                        "long_name": route.long_name,
+                        "times": times })
 
     return HttpResponse(simplejson.dumps({ "stops": [ {'name': stop.name, 
                                                        'code': stop.stop_code,
@@ -73,13 +104,6 @@ def stoptimes_in_range(request, location, secs):
     import routez
     graph = routez.travel.graph
 
-    today = datetime.date.today()
-    service_period = 'weekday'
-    if today.weekday() == 5:
-        service_period = 'saturday'
-    elif today.weekday() == 6:
-        service_period = 'sunday'
-
     # checking 500 meters. FIXME: make configurable
     tstops = graph.find_tripstops_in_range(latlng[0], latlng[1], TripStop.GTFS,
                                            500)
@@ -93,7 +117,8 @@ def stoptimes_in_range(request, location, secs):
     for ts in tstops:
         distance_to_stop = latlng_dist(latlng[0], latlng[1], ts.lat, ts.lng)
         distance_to_stop_hash[ts.id] = distance_to_stop
-        for route_id in ts.get_routes(service_period):
+
+        for route_id in get_route_ids_for_stop(graph, ts.id, secs):
             if not routehash.get(route_id) or \
                     routehash[route_id][0] > distance_to_stop:
                 routehash[route_id] = (distance_to_stop, ts)
@@ -110,9 +135,8 @@ def stoptimes_in_range(request, location, secs):
         stop = Stop.objects.filter(stop_id=id)[0]
         routesjson = []
         for route_id in stophash[id]:
-            ts = graph.get_tripstop(id)
-            thops = ts.find_triphops(int(secs), int(route_id), service_period,
-                                     3)
+            thops = find_triphops_for_stop(graph, id, route_id, secs, 3)
+
             if len(thops):
                 times = []
                 for thop in thops:
