7501d5e9710e7ee82857b35c4fe0f637f923d4ac
William Lachance <wrlach@gmail.com>
2009-10-06 15:10:52 -0400

    Specify db to use for geocode command line utility

--- geocode.cc
+++ geocode.cc
@@ -6,9 +6,14 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    GeoCoder g("test.db");
+    if (argc < 3)
+    {
+        printf("USAGE: %s <geodb> <expression>\n", argv[0]);
+    }
 
-    pair<float, float> latlng = g.get_latlng(argv[1]);
+    GeoCoder g(argv[1]);
+
+    pair<float, float> latlng = g.get_latlng(argv[2]);
     printf("%f, %f\n", latlng.first, latlng.second);
 
     return 0;
