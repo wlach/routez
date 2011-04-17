16119db6ff0d857f7dab755878d12ab490f53717
William Lachance <wrlach@gmail.com>
2011-04-17 14:35:39 -0400

    Remove debugging setting from settings.py
    We want to set that in local settings

--- src/routez/settings.py
+++ src/routez/settings.py
@@ -3,9 +3,6 @@
 import os
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = True 
-TEMPLATE_DEBUG = DEBUG
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
