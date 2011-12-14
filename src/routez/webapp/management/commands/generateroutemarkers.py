93a274a085495153f89723f3694763474e4ce151
William Lachance <wlachance@mozilla.com>
2011-12-14 10:27:00 -0500

    Don't error out if we don't have a marker for a route type

--- src/routez/webapp/management/commands/generateroutemarkers.py
+++ src/routez/webapp/management/commands/generateroutemarkers.py
@@ -10,14 +10,16 @@ class Command(BaseCommand):
         marker_basepath = settings.PROJECT_PATH + "/webapp/static/images/"
         generated_path = settings.PROJECT_PATH + "/webapp/static/images/generated/"
         for route in Route.objects.all():
+            marker_basename = None
             if route.type == 3:
                 marker_basename = "marker_bus"
             elif route.type == 4:
                 marker_basename = "marker_ferry"
 
-            ret = os.system('convert %s -weight Bold -annotate +34+14 %s %s' %
-                            (marker_basepath+ marker_basename + ".png", route.short_name, 
-                             generated_path + marker_basename + route.short_name + ".png"))
-            if ret != 0:
-                raise CommandError('Error writing marker with route %s' % route.short_name)
+            if marker_basename:
+                ret = os.system('convert %s -weight Bold -annotate +34+14 %s %s' %
+                                (marker_basepath+ marker_basename + ".png", route.short_name, 
+                                 generated_path + marker_basename + route.short_name + ".png"))
+                if ret != 0:
+                    raise CommandError('Error writing marker with route %s' % route.short_name)
 
